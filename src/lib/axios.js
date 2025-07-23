420ff3fec07a55949e4771910bcabdf337f4304c
Ahmad Raza <ahmadsajidmughal357@gmail.com>
2025-07-24 02:42:17 +0500

    again fix axios baseURL

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.DEV
-        ? "http://localhost:5001/api"
-        : "https://real-chat-app-backend-production.up.railway.app/api",
+    baseURL: "https://real-chat-app-backend-production.up.railway.app/api",
     withCredentials: true,
 });
