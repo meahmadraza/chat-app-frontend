d204459c37f795ffa67989008d7f9d86e106dc73
Ahmad Raza <ahmadsajidmughal357@gmail.com>
2025-07-24 02:30:16 +0500

    updated axios base URL

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: import.meta.env.DEV
+        ? "http://localhost:5001/api"
+        : "https://real-chat-app-backend-production.up.railway.app/api",
     withCredentials: true,
-})
+});
