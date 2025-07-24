93ba725b39b737b456dd85f31bec4d7d3524650e
Ahmad Raza <ahmadsajidmughal357@gmail.com>
2025-07-24 16:18:28 +0500

    changing in axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,4 +3,9 @@ import axios from 'axios';
 export const axiosInstance = axios.create({
     baseURL: "https://real-chat-app-backend-production.up.railway.app/api",
     withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Credentials': 'true'
+    }
 });
