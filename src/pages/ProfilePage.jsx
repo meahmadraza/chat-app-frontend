b333d3799b056b279c85a011698fd53a4221ed11
Ahmad Raza <ahmadsajidmughal357@gmail.com>
2025-07-24 14:35:06 +0500

    change padding in profile.jsx

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -22,7 +22,7 @@ const ProfilePage = () => {
     return (
         <>
             <Sidebar />
-            <div className="pt-16 h-screen">
+            <div className="pt-16 h-auto">
                 <div className="max-w-2xl mx-auto p-4 py-8">
                     <div className="bg-base-300 rounded-xl p-8 space-y-8">
 
