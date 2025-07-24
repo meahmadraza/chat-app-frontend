24df8d06d5ef7b34267c49cc95aeb363863de64f
Ahmad Raza <ahmadsajidmughal357@gmail.com>
2025-07-24 14:16:37 +0500

    Update chatStore.js

--- src/Store/useChatStore.js
+++ src/Store/useChatStore.js
@@ -15,7 +15,7 @@ export const useChatStore = create((set, get) => ({
             const res = await axiosInstance.get('/messages/users')
             set({ users: res.data })
         } catch (error) {
-            toast.error('Error fetching users', error.data.message)
+            toast.error(error?.response?.data?.message || 'Error fetching users')
         } finally {
             set({ isUserLoading: false })
         }
@@ -27,19 +27,19 @@ export const useChatStore = create((set, get) => ({
             const res = await axiosInstance.get(`/messages/${userId}`)
             set({ messages: res.data })
         } catch (error) {
-            toast.error('Error fetching messages', error.data.message)
+            toast.error(error?.response?.data?.message || 'Error fetching messages')
         } finally {
             set({ isMessagesLoading: false })
         }
     },
+
     sendMessages: async (messageData) => {
         const { selectedUser, messages } = get();
         try {
             const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData)
             set({ messages: [...messages, res.data] })
         } catch (error) {
-            toast.error('Error sending message', error.data.message)
-
+            toast.error(error?.response?.data?.message || 'Error sending message')
         }
     },
 
